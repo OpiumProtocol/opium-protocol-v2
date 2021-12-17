8a1b1fff4f58e1899403095ba8cfe00d11e2ea86
RiccardoBiosas <rbios@protonmail.com>
2021-12-17 13:43:59 +0100

    removed TestOracleAggregatorUpgrade and related documentation

--- contracts/test/upgradeability/TestOracleAggregatorUpgrade.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity 0.8.5;
-import "../../core/OracleAggregator.sol";
-
-contract TestOracleAggregatorUpgrade is OracleAggregator {
-    function placeholder() external pure returns (string memory) {
-        return "upgraded";
-    }
-}
