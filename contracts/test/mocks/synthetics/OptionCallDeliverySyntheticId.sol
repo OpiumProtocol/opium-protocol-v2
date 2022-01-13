67efc269013cacf3bdc3f39ace126c5d83ad55d0
RiccardoBiosas <rbios@protonmail.com>
2022-01-13 11:28:50 +0100

    changed mocks/synthetics directory structure

--- contracts/test/mocks/synthetics/OptionCallDeliverySyntheticId.sol
+++ contracts/test/mocks/synthetics/OptionCallDeliverySyntheticId.sol
@@ -2,26 +2,22 @@
 pragma solidity 0.8.5;
 
 import "openzeppelin-solidity/contracts/access/Ownable.sol";
+import "../../../interfaces/IDerivativeLogic.sol";
 
-import "../../../../interfaces/IDerivativeLogic.sol";
-
-contract RatioCdsSyntheticId is IDerivativeLogic, Ownable {
+contract OptionCallDeliverySyntheticId is IDerivativeLogic, Ownable {
   address private author;
   uint256 private commission;
-
-  uint256 constant public TRIGGER_BASE = 1e18;
-
 
   constructor(address _author, uint256 _commission) {
     /*
     {
       "author": "Opium.Team",
-      "type": "swap",
-      "subtype": "cds",
-      "description": "Ratio CDS logic contract"
+      "type": "option",
+      "subtype": "call",
+      "description": "Option Call with delivery logic contract"
     }
     */
-    emit LogMetadataSet("{\"author\":\"Opium.Team\",\"type\":\"swap\",\"subtype\":\"cds\",\"description\":\"Ratio CDS logic contract\"}");
+    emit LogMetadataSet("{\"author\":\"Opium.Team\",\"type\":\"option\",\"subtype\":\"call\",\"description\":\"Option Call with delivery logic contract\"}");
     
     author = _author;
     commission = _commission;
@@ -29,20 +25,19 @@ contract RatioCdsSyntheticId is IDerivativeLogic, Ownable {
 
   /// @return Returns the custom name of a derivative ticker which will be used as part of the name of its positions
   function getSyntheticIdName() external pure override returns (string memory) {
-    return "CDS";
+    return "OPT-C";
   }
 
-  // params[0] - Trigger
-  // params[1] - Fixed Premium
-  function validateInput(LibDerivative.Derivative calldata _derivative) external view override returns (bool) {
+  // params[0] - Strike price
+  // params[1] - fixedPremium
+  function validateInput(LibDerivative.Derivative calldata _derivative) public view override returns (bool) {
     return (
       // Derivative
       _derivative.endTime > block.timestamp &&
       _derivative.margin > 0 &&
       _derivative.params.length == 2 &&
 
-      _derivative.params[0] > 0 && // Trigger
-      _derivative.params[0] <= TRIGGER_BASE // Trigger
+      _derivative.params[0] > 0 // Strike price > 0
     );
   }
 
@@ -54,26 +49,32 @@ contract RatioCdsSyntheticId is IDerivativeLogic, Ownable {
   }
 
   function getExecutionPayout(LibDerivative.Derivative calldata _derivative, uint256 _result) external pure override returns (uint256 buyerPayout, uint256 sellerPayout) {
-    uint256 trigger = _derivative.params[0];
+    uint256 strikePrice = _derivative.params[0];
     uint256 fixedPremium = _derivative.params[1];
     uint256 nominal = _derivative.margin;
 
-    if (_result < trigger) {
-      sellerPayout = nominal * _result / TRIGGER_BASE;
-      buyerPayout = nominal - sellerPayout;
+    if (_result > strikePrice) {
+      // buyer payout = margin * (result - strike price) / result
+      buyerPayout = nominal * (_result - strikePrice) / _result;
+
+      // seller payout = margin - buyer payout
+      sellerPayout = nominal - buyerPayout;
     } else {
+      // buyer payout = 0
       buyerPayout = 0;
+      
+      // seller payout = margin
       sellerPayout = nominal;
     }
 
-    // Add premium
+    // Add fixed premium to seller payout
     sellerPayout = sellerPayout + fixedPremium;
   }
 
   /** COMMISSION */
   /// @notice Getter for syntheticId author address
   /// @return address syntheticId author address
-  function getAuthorAddress() external view override returns (address) {
+  function getAuthorAddress() public view override returns (address) {
     return author;
   }
 
@@ -88,11 +89,10 @@ contract RatioCdsSyntheticId is IDerivativeLogic, Ownable {
     return true;
   }
 
-  function allowThirdpartyExecution(bool) external pure override {
-  }
+  function allowThirdpartyExecution(bool) external override {}
 
   /** GOVERNANCE */
-  function setAuthorAddress(address _author) external onlyOwner {
+  function setAuthorAddress(address _author) public onlyOwner {
     require(_author != address(0), "Can't set to zero address");
     author = _author;
   }
--- contracts/test/mocks/synthetics/RatioCdsSyntheticId.sol
+++ contracts/test/mocks/synthetics/RatioCdsSyntheticId.sol
@@ -3,22 +3,25 @@ pragma solidity 0.8.5;
 
 import "openzeppelin-solidity/contracts/access/Ownable.sol";
 
-import "../../../../interfaces/IDerivativeLogic.sol";
+import "../../../interfaces/IDerivativeLogic.sol";
 
-contract OptionCallDeliverySyntheticId is IDerivativeLogic, Ownable {
+contract RatioCdsSyntheticId is IDerivativeLogic, Ownable {
   address private author;
   uint256 private commission;
+
+  uint256 constant public TRIGGER_BASE = 1e18;
+
 
   constructor(address _author, uint256 _commission) {
     /*
     {
       "author": "Opium.Team",
-      "type": "option",
-      "subtype": "call",
-      "description": "Option Call with delivery logic contract"
+      "type": "swap",
+      "subtype": "cds",
+      "description": "Ratio CDS logic contract"
     }
     */
-    emit LogMetadataSet("{\"author\":\"Opium.Team\",\"type\":\"option\",\"subtype\":\"call\",\"description\":\"Option Call with delivery logic contract\"}");
+    emit LogMetadataSet("{\"author\":\"Opium.Team\",\"type\":\"swap\",\"subtype\":\"cds\",\"description\":\"Ratio CDS logic contract\"}");
     
     author = _author;
     commission = _commission;
@@ -26,19 +29,20 @@ contract OptionCallDeliverySyntheticId is IDerivativeLogic, Ownable {
 
   /// @return Returns the custom name of a derivative ticker which will be used as part of the name of its positions
   function getSyntheticIdName() external pure override returns (string memory) {
-    return "OPT-C";
+    return "CDS";
   }
 
-  // params[0] - Strike price
-  // params[1] - fixedPremium
-  function validateInput(LibDerivative.Derivative calldata _derivative) public view override returns (bool) {
+  // params[0] - Trigger
+  // params[1] - Fixed Premium
+  function validateInput(LibDerivative.Derivative calldata _derivative) external view override returns (bool) {
     return (
       // Derivative
       _derivative.endTime > block.timestamp &&
       _derivative.margin > 0 &&
       _derivative.params.length == 2 &&
 
-      _derivative.params[0] > 0 // Strike price > 0
+      _derivative.params[0] > 0 && // Trigger
+      _derivative.params[0] <= TRIGGER_BASE // Trigger
     );
   }
 
@@ -50,32 +54,26 @@ contract OptionCallDeliverySyntheticId is IDerivativeLogic, Ownable {
   }
 
   function getExecutionPayout(LibDerivative.Derivative calldata _derivative, uint256 _result) external pure override returns (uint256 buyerPayout, uint256 sellerPayout) {
-    uint256 strikePrice = _derivative.params[0];
+    uint256 trigger = _derivative.params[0];
     uint256 fixedPremium = _derivative.params[1];
     uint256 nominal = _derivative.margin;
 
-    if (_result > strikePrice) {
-      // buyer payout = margin * (result - strike price) / result
-      buyerPayout = nominal * (_result - strikePrice) / _result;
-
-      // seller payout = margin - buyer payout
-      sellerPayout = nominal - buyerPayout;
+    if (_result < trigger) {
+      sellerPayout = nominal * _result / TRIGGER_BASE;
+      buyerPayout = nominal - sellerPayout;
     } else {
-      // buyer payout = 0
       buyerPayout = 0;
-      
-      // seller payout = margin
       sellerPayout = nominal;
     }
 
-    // Add fixed premium to seller payout
+    // Add premium
     sellerPayout = sellerPayout + fixedPremium;
   }
 
   /** COMMISSION */
   /// @notice Getter for syntheticId author address
   /// @return address syntheticId author address
-  function getAuthorAddress() public view override returns (address) {
+  function getAuthorAddress() external view override returns (address) {
     return author;
   }
 
@@ -90,10 +88,11 @@ contract OptionCallDeliverySyntheticId is IDerivativeLogic, Ownable {
     return true;
   }
 
-  function allowThirdpartyExecution(bool) external override {}
+  function allowThirdpartyExecution(bool) external pure override {
+  }
 
   /** GOVERNANCE */
-  function setAuthorAddress(address _author) public onlyOwner {
+  function setAuthorAddress(address _author) external onlyOwner {
     require(_author != address(0), "Can't set to zero address");
     author = _author;
   }
